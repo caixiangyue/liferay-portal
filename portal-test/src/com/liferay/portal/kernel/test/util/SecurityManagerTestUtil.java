d0059ba8b7fd606e6540c8eef9083f49509b181f
caixiangyue <xiangyue.cai@liferay.com>
2019-01-16 18:17:53 +0800

    LPS-X add SecurityManagerTestUtil

---
+++ portal-test/src/com/liferay/portal/kernel/test/util/SecurityManagerTestUtil.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.test.util;
+
+import com.liferay.petra.reflect.ReflectionUtil;
+
+import java.security.Permission;
+
+/**
+ * @author Xiangyue Cai
+ */
+public class SecurityManagerTestUtil {
+
+	public static SecurityManager
+			getSecurityManagerAndSetDeclaredMethodException(
+				RuntimeException exception)
+		throws ClassNotFoundException {
+
+		Class.forName(ReflectionUtil.class.getName());
+
+		SecurityManager securityManager = System.getSecurityManager();
+
+		System.setSecurityManager(
+			new SecurityManager() {
+
+				@Override
+				public void checkPermission(Permission permission) {
+					if ("suppressAccessChecks".equals(permission.getName())) {
+						throw exception;
+					}
+				}
+
+			});
+
+		return securityManager;
+	}
+
+	public static void setSecutityManager(SecurityManager securityManager) {
+		System.setSecurityManager(securityManager);
+	}
+
+}
